c4df8f3c6c0bbd1cef84d4cb12070cb9e3fadbdd
JaredLittle <jlittle6558@students.mc3.edu>
2013-02-06 10:26:09 -0800

    Create ImagePanel.java

---
+++ ImagePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JPanel {
+  
+	JTextArea data;
+		
+	public ImagePanel() {
+		
+		// Sets up a JPanel that holds the drawing area
+		DrawPanel drawPanel = new DrawPanel();
+
+	}
+}
